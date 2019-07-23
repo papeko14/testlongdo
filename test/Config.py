24f610c07b9b6b633d7f2a74bb7d3cd79dab6bd2
thanopath <44945805+papeko14@users.noreply.github.com>
2019-07-23 11:58:18 +0700

    Add files via upload

---
+++ test/Config.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+Channel_secret = "65fec5097bdf06101689d1ed30e282d0"
+
+Channel_access_token = "ENcqjwF/NJjmLRxvEguzkDDT8YpTX2w85jwPzlFhwRh7/qflNYnH7vVkyggqDVwQUJg7QD+VD2YRkJ56d3RSCex/CeHAmqLAybjGAD/bnXgJ0Ci+ijkVE9LfQBQm+2AI7FZE8PsuRCE5cdynYzIVhwdB04t89/1O/w1cDnyilFU="
+
+
+
+Basic_ID = "@808gcgwv"
